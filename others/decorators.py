7682c903c3a29282e1ae6519dda371b399cb8319
EmilianStoyanov <emilian.stoyanov@outlook.com>
2022-04-25 15:01:09 +0300

    tasks

---
+++ others/decorators.py
@@ -0,0 +1,19 @@
+import os
+
+os.system("cls")
+
+
+def logger(fn):
+    def wrapper(*args):
+        print(fn.__name__, (args[0]), (args[1]))
+        return fn(*args)
+
+    return wrapper
+
+
+@logger
+def sum(a, b):
+    return a + b
+
+
+print(sum(1, 1))
